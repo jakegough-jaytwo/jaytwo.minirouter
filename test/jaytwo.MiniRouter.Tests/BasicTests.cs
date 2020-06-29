6d351db416a91a45f84664538ae801d4844e8dc8
Jake Gough <jakegough@gmail.com>
2020-06-28 18:55:58 -0600

    normalized sourcelink, stylecop, and coverlet/cobertura

---
+++ test/jaytwo.MiniRouter.Tests/BasicTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace jaytwo.MiniRouter.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void HelloWorld()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.NotEmpty("Hello World");
+        }
+    }
+}
